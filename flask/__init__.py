8e12f534b7ee717d954da4c47e40373d43ee7048
Nut <your_email@youremail.com>
2020-12-11 16:18:36 +0700

    webpage

--- flask/__init__.py
+++ flask/__init__.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def home():
     return "<h1> Home </h1>"
 
+@app.route("/gallery")
+def gallery():
+    return "<h1> Gallery </h1>"
+
+
+@app.route("/login")
+def login():
+    return "<h1> login </h1>"
+
+
+@app.route("/user")
+def user():
+    return "<h1> user </h1>"
+
 
 
 if __name__ ==  "__main__":
