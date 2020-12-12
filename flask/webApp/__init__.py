6d3a1b9c5d5c21e7f07f3e93821295d46de09ec6
Nut <your_email@youremail.com>
2020-12-12 16:59:00 +0700

    AL backup

--- flask/webApp/__init__.py
+++ flask/webApp/__init__.py
@@ -1,27 +1,36 @@
 from  flask import Flask,render_template
 
+
+
 app = Flask(__name__)
 
+#main
 @app.route("/")
-@app.route("/home")
-def main():
-    return "<h1> Home </h1>"
-
-@app.route("/gallery")
-def gallery():
-    return "<h1> Gallery </h1>"
+@app.route("/main")
+def home():
+    return render_template("main.html")
 
 
+#login
 @app.route("/login")
 def login():
-    return "<h1> login </h1>"
+    return render_template("login.html")
 
 
+#logout
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+
+#user
 @app.route("/user")
 def user():
     return "<h1> user </h1>"
 
+#gallery
 
 
+#run
 if __name__ ==  "__main__":
     app.run(debug = True)
